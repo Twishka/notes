1a4d02ccd200aea5299ec631a194304c33fd09ae
Iryna Sadovnykova <sadovnikova.irina@gmail.com>
2018-12-18 02:39:55 +0200

    Updates

---
+++ precache-manifest.99e431fc8608bb864fef6cefad13ffda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/notes/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "1291e1b612bc298497c9",
+    "url": "/notes/static/js/runtime~main.1291e1b6.js"
+  },
+  {
+    "revision": "4988b90384c21102d02f",
+    "url": "/notes/static/js/main.4988b903.chunk.js"
+  },
+  {
+    "revision": "ee54deecc4010b09b6f1",
+    "url": "/notes/static/js/1.ee54deec.chunk.js"
+  },
+  {
+    "revision": "4988b90384c21102d02f",
+    "url": "/notes/static/css/main.26923d4c.chunk.css"
+  },
+  {
+    "revision": "49e4ba0a982e35ed92872fa82f11ff66",
+    "url": "/notes/index.html"
+  }
+];
